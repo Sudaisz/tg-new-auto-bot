b0b6ffae3b268042070272197cff322dcff44caa
Sudais main <sudaisabubakar6754@gmail.com>
2025-07-14 02:33:04 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+BOT_TOKEN = "8080511650:AAGR9lCQuHt3f2mWsYQMudFSCn7MP0uZ72Y"
+ADMIN_ID = 6619154186  # Replace with your Telegram numeric ID
+
+REQUIRED_CHANNELS = [
+    "t.me/beyondtech_v2",
+    "t.me/suxedo_ban",
+    "https://t.me/+azx1_D4u5j0wYTE0",
+    "t.me/kelvintechnologies"
